9bc9009ec17d12744e6054ffdc136daf545a46b5
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-07 20:13:22 +0300

    mark up

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+// console.log(galleryItems);
+const container = document.querySelector(".gallery");
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${preview}">
+            <img
+              class="gallery__image"
+              src="${original}" 
+              alt="${description}" 
+              data-source="${preview}">
+          </a>
+        </li>`
+    )
+    .join("");
+}
+container.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+const imageLink = document.querySelectorAll(".gallery__link");
+console.log(imageLink);
+imageLink.forEach((item) => item.addEventListener("click", handlerClick));
+function handlerClick(evt) {
+  evt.preventDefault();
+}
