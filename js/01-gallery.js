e2e3712464e76b732e65572a2c93eae735b005b8
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-08 19:14:59 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,26 +2,33 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 // console.log(galleryItems);
 const container = document.querySelector(".gallery");
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({ preview, original, description }) =>
-        `<li class="gallery__item">
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
           <a class="gallery__link" href="${preview}">
             <img
               class="gallery__image"
               src="${original}" 
               alt="${description}" 
-              data-source="${preview}">
+              data-source="${preview}"
+              loading="lazy">
           </a>
         </li>`
-    )
-    .join("");
-}
-container.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
-const imageLink = document.querySelectorAll(".gallery__link");
-console.log(imageLink);
-imageLink.forEach((item) => item.addEventListener("click", handlerClick));
+  )
+  .join("");
+container.insertAdjacentHTML("afterbegin", markup);
+const link = document.querySelectorAll(".gallery__link");
+container.addEventListener("click", handlerClick);
 function handlerClick(evt) {
   evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  const galerySource = evt.target.dataset.source;
+  const product = galleryItems.find(({ preview }) => preview === galerySource);
+  const instance = basicLightbox.create(
+    `<img src="${product.preview}" alt="${product.description}">`
+  );
+  instance.show();
 }
